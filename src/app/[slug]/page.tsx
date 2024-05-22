ed5ddd0ee860df18c7d9bacf57d0fac4fc41e1af
bashir <bashirahmed.cse@gmail.com>
2024-05-22 17:41:59 +0600

    first commit

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignlePage = () => {
+  return (
+    <div>SignlePage</div>
+  )
+}
+
+export default SignlePage
---
+++ src/app/list/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListPage = () => {
+  return (
+    <div className=''>ListPage</div>
+  )
+}
+
+export default ListPage
